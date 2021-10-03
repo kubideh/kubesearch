2b78546f3d8a22476a95d54675ec7cf903615cdb
Evan McClure <evanmcclure@gmail.com>
2021-10-02 17:39:05 -0700

    Bootstrap the project by adding a plan and a simple TDD starting point

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# kubesearch
+kubesearch
+---
+
 Fulltext search for Kubernetes
+
+## Plan
+
+Build a [custom Kubernetes controller](https://github.com/kubernetes/sample-controller/blob/master/docs/controller-client-go.md) that indexes the metadata of Kubernetes objects into an [inverted index that supports fulltext search](https://github.com/blevesearch/bleve). The goal is to let the user query for their objects without having to know the exact labels. The result will be a list of object (kind, namespace, name) tuples with which the user can use kubectl to easily get their desired object.
+
+A stretch goal is to build a kubectl plugin that lets formats the API output of kubesearch and enhances the user experience.
---
+++ hello_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHello(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost/v1/search", nil)
+	writer := httptest.NewRecorder()
+
+	func(writer http.ResponseWriter, request *http.Request) {
+		io.WriteString(writer, "<html><body>Hello World!</body></html>")
+	}(writer, request)
+
+	response := writer.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, http.StatusOK, response.StatusCode)
+	assert.Equal(t, "text/html; charset=utf-8", response.Header.Get("Content-Type"))
+	assert.Equal(t, "<html><body>Hello World!</body></html>", string(body))
+}
