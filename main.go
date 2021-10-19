1bfbbbf7e859a1ac2579ce050b3f40def7c93882
Evan McClure <evanmcclure@gmail.com>
2021-10-19 15:50:37 -0700

    Wire up hello

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/kubideh/kubesearch/hello"
+	_ "github.com/kubideh/kubesearch/hello"
 )
 
 func main() {
 	log.Println("Hello World!")
 
-	if err := http.ListenAndServe(":8080", http.HandlerFunc(hello.Handler)); err != nil {
+	if err := http.ListenAndServe(":8080", http.DefaultServeMux); err != nil {
 		log.Fatalln(err)
 	}
 }
