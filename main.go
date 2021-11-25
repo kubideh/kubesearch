94542e61ea9f9f0b7019fb0c07b09d84ba3b2673
Evan McClure <evanmcclure@gmail.com>
2021-11-25 09:53:23 -0800

    Refactor

--- main.go
+++ main.go
@@ -15,25 +15,10 @@ import (
 )
 
 func main() {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
+	kubeconfig := defineKubeconfigFlag()
 	flag.Parse()
 
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		klog.Fatalln(err)
-	}
-
-	// create the clientset
-	client, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		klog.Fatalln(err)
-	}
+	client := createKubernetesClient(kubeconfig)
 
 	// create the Controller to be used by the search API handler
 	controller := search.NewController(client)
@@ -50,4 +35,29 @@ func main() {
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		klog.Fatalln(err)
 	}
+}
+
+func defineKubeconfigFlag() (kubeconfig *string) {
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	return
+}
+
+func createKubernetesClient(kubeconfig *string) *kubernetes.Clientset {
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		klog.Fatalln(err)
+	}
+
+	// create the clientset
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		klog.Fatalln(err)
+	}
+
+	return client
 }
