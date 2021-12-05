103269868ab24d0f553299b3d636ee34a66d4dc9
Evan McClure <evanmcclure@gmail.com>
2021-12-05 11:15:22 -0800

    Refactor

---
+++ search/app/app.go
@@ -0,0 +1,75 @@
+package app
+
+import (
+	"flag"
+	"net/http"
+	"path/filepath"
+
+	"github.com/kubideh/kubesearch/search/api"
+	"github.com/kubideh/kubesearch/search/controller"
+	"github.com/kubideh/kubesearch/search/index"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"k8s.io/klog/v2"
+)
+
+// New returns App objects.
+func New() App {
+	return App{
+		kubeconfig: kubeconfigFlag(),
+	}
+}
+
+func kubeconfigFlag() (kubeconfig *string) {
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	return
+}
+
+// App provides everything needed to run kubesearch.
+type App struct {
+	kubeconfig *string
+}
+
+// Run creates a controller that uses the given kubeconfig to watch
+// for changes to Kubernetes objects, indexes those objects, and
+// provides an API for searching for those objects.
+func (a App) Run() {
+	flag.Parse()
+
+	// create the Controller to be used by the search API handler
+	cont := controller.New(a.client())
+
+	idx := index.New()
+
+	cancel := cont.Start(idx)
+	defer cancel()
+
+	mux := http.NewServeMux()
+	api.RegisterHandler(mux, idx, cont.Store())
+
+	klog.Infoln("Listening on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		klog.Fatalln(err)
+	}
+}
+
+func (a App) client() *kubernetes.Clientset {
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *a.kubeconfig)
+	if err != nil {
+		klog.Fatalln(err)
+	}
+
+	// create the clientset
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		klog.Fatalln(err)
+	}
+
+	return client
+}
